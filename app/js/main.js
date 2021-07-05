6716070e96395bc2bfb2b049a6237d2210f42de4
Nikolay Miller <miller.ni.vl@gmail.com>
2021-07-05 20:41:02 +0300

    login page, about page, home page completed

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,20 @@
 $(function () {
+	$('.intro__slider').slick({
+		arrows: false,
+		dots: true
+	})
 
+	$('.filter__btn--products').on('click', function () {
+		$('.filter__btn--products').removeClass('_active');
+		$(this).addClass('_active');
+	})
+
+	$('.filter__btn--category').on('click', function () {
+		$('.filter__btn--category').removeClass('_active');
+		$(this).addClass('_active');
+	})
+
+
+
+	let mixer = mixitup('.products__items');
 })
